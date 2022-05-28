d1982e6d69278787e589ff77f317b6aae2371ad8
Dat Nguyen <dnguyenk2@gmail.com>
2022-05-27 20:01:35 -0700

    Completed Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function NavBar() {
+function NavBar(){
   return (
     <nav>
       <a href="#home">I'm a link!</a>
@@ -8,7 +8,7 @@ function NavBar() {
   );
 }
 
-function Home() {
+function Home(){
   return (
     <div id="home">
       <h1>Home</h1>
@@ -16,13 +16,21 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
-
+/* write an <About> component here */
+function About(){
+  return (
+    <div id="about">
+      <h2>About</h2>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Home/>
+      <About />
       {/* add the <Home> component here */}
       {/* add your <About> component here */}
     </div>
